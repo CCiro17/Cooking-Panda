6f965790aa06f979e50647c85133def91b372fc8
CCiro17 <84789943+CCiro17@users.noreply.github.com>
2025-04-16 07:50:55 +0200

    Update App_FoundationApp.swift
    Added credits

--- App Foundation/App Foundation/App_FoundationApp.swift
+++ App Foundation/App Foundation/App_FoundationApp.swift
@@ -2,7 +2,7 @@
 //  App_FoundationApp.swift
 //  App Foundation
 //
-//  Created by Fabio Fontana on 09/04/25.
+//  Created by Fabio Fontana, Adriana Palmese, Ciro Cafora, Angelica Sannino, Alessandro Maddaloni and Ali Raza on 09/04/25.
 //
 
 import SwiftUI
